604a19ff98ba14e4963af998f28edd6d68be042c
gandharv <gandharv11047@iiitd.ac.in>
2015-03-14 03:48:09 +0530

    Seprate functionality

--- src/main/java/com/example/RRCpacketSniffer/MainActivity.java
+++ src/main/java/com/example/RRCpacketSniffer/MainActivity.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 
 import android.content.ComponentName;
@@ -22,17 +23,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	Intent measure_intent ;
 	PackageManager packageManager;
 	Button button1;
 	Button button2;
+    Spinner spinner2;
     String result;
+    int currentpos = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,103 +48,233 @@ public class MainActivity extends ActionBarActivity {
 		packageManager = this.getPackageManager();
 		button1 = (Button) findViewById(R.id.button1);
 		button2 = (Button) findViewById(R.id.button2);
+        spinner2 = (Spinner)findViewById(R.id.spinner2);
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> parentView,
+                                       View selectedItemView, int position, long id) {
+                // Object item = parentView.getItemAtPosition(position);
+
+                currentpos = spinner2.getSelectedItemPosition();
+                Log.d("action",currentpos+"");
+
+            }
+
+            public void onNothingSelected(AdapterView<?> arg0) {// do nothing
+            }
+
+        });
         final Process  process;
 		
 		button1.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View arg0) {
 
-				//String ussdCode = "%2A" + Uri.encode("#") + "0011" + Uri.encode("#");
-				String ussdCode = "%2A"+"%23"+"0011"+"%23";
-                RunPacketCapture();
-				if (packageManager != null) {
-				    measure_intent = new Intent(Intent.ACTION_MAIN).
-				            addCategory(Intent.CATEGORY_LAUNCHER).setComponent(new ComponentName(
-				                    "com.sec.android.app.servicemodeapp",
-				                    "com.sec.android.app.servicemodeapp.ServiceModeApp"));
-				    measure_intent.putExtra("keyString", "0011");
-				    ResolveInfo resolved = packageManager.resolveActivity(
-				            measure_intent, PackageManager.MATCH_DEFAULT_ONLY);
-				    if (resolved != null) {
-				        startActivity(measure_intent);
-				        return;
-				    } else {
-				        Toast.makeText(getApplicationContext(),"version not implemented", Toast.LENGTH_LONG).show();
-				    }
-				}
-				Toast t = Toast.makeText(getApplicationContext(),
-						"Sniffer started", Toast.LENGTH_SHORT);
-				t.show();
-			}
+                switch (currentpos) {
 
+                    case 0:
+                    Log.d("ACTION:","BOTH");
+                    RunPacketCapture();
+                    if (packageManager != null) {
+                        measure_intent = new Intent(Intent.ACTION_MAIN).
+                                addCategory(Intent.CATEGORY_LAUNCHER).setComponent(new ComponentName(
+                                "com.sec.android.app.servicemodeapp",
+                                "com.sec.android.app.servicemodeapp.ServiceModeApp"));
+                        measure_intent.putExtra("keyString", "0011");
+                        ResolveInfo resolved = packageManager.resolveActivity(
+                                measure_intent, PackageManager.MATCH_DEFAULT_ONLY);
+                        if (resolved != null) {
+                            startActivity(measure_intent);
+                            return;
+                        } else {
+                            Toast.makeText(getApplicationContext(), "version not implemented", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Both Sniffer started", Toast.LENGTH_SHORT);
+                    t.show();
+                    break;
+                    case 1:
+                        Log.d("ACTION:","PACKET");
+                        RunPacketCapture();
+                        Toast t1 = Toast.makeText(getApplicationContext(),
+                                "Packet Sniffer started", Toast.LENGTH_SHORT);
+                        t1.show();
+                        break;
+                    case 2:
+                        Log.d("ACTION:","RRC");
+                        if (packageManager != null) {
+                            measure_intent = new Intent(Intent.ACTION_MAIN).
+                                    addCategory(Intent.CATEGORY_LAUNCHER).setComponent(new ComponentName(
+                                    "com.sec.android.app.servicemodeapp",
+                                    "com.sec.android.app.servicemodeapp.ServiceModeApp"));
+                            measure_intent.putExtra("keyString", "0011");
+                            ResolveInfo resolved = packageManager.resolveActivity(
+                                    measure_intent, PackageManager.MATCH_DEFAULT_ONLY);
+                            if (resolved != null) {
+                                startActivity(measure_intent);
+                                return;
+                            } else {
+                                Toast.makeText(getApplicationContext(), "version not implemented", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        Toast t2 = Toast.makeText(getApplicationContext(),
+                                "RRC Sniffer started", Toast.LENGTH_SHORT);
+                        t2.show();
+                        break;
+                }
+            }
 		});
 
-		
 		button2.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View arg0) {
-					
-				 Runnable runnable = new Runnable() {
-				      @Override
-				      public void run() {
-				    	  BufferedWriter bw = null;
-							String line;
-							String outText;
-							int i;
-							try {
-								File file1 = new File("mnt/sdcard/dump.txt");
-								file1.createNewFile();
 
-								OutputStreamWriter os = new OutputStreamWriter(
-										new FileOutputStream(file1));
+                switch (currentpos) {
 
-								bw = new BufferedWriter(os);
+                    case 0:
+                    Log.d("CLOSING","BOTH");
+                    Runnable runnable = new Runnable() {
+                        @Override
+                        public void run() {
+                            BufferedWriter bw = null;
+                            String line;
+                            String outText;
+                            int i;
+                            try {
+                                File file1 = new File("mnt/sdcard/dump.txt");
+                                file1.createNewFile();
 
-								Process process = Runtime.getRuntime().exec(
-										"logcat -v threadtime -s ServiceMode");
-								int length = process.toString().length();
+                                OutputStreamWriter os = new OutputStreamWriter(
+                                        new FileOutputStream(file1));
 
-								System.out.println("Length" + length);
+                                bw = new BufferedWriter(os);
 
-								BufferedReader reader = new BufferedReader(
-										new InputStreamReader(process.getInputStream()));
+                                Process process = Runtime.getRuntime().exec(
+                                        "logcat -v threadtime -s ServiceMode");
+                                int length = process.toString().length();
 
-								StringBuffer output1 = new StringBuffer();
+                                System.out.println("Length" + length);
 
-								while ((line = reader.readLine()) != null) {
+                                BufferedReader reader = new BufferedReader(
+                                        new InputStreamReader(process.getInputStream()));
 
-									bw.write(line);
-									bw.newLine();
+                                StringBuffer output1 = new StringBuffer();
 
-								}
+                                while ((line = reader.readLine()) != null) {
 
-							} catch (IOException e) {
-							} finally {
+                                    bw.write(line);
+                                    bw.newLine();
 
-								try {
-									if (bw != null) {
-										bw.close();
+                                }
 
-										Toast.makeText(getBaseContext(),
-												"Done writing to dump file",
-												Toast.LENGTH_SHORT).show();
+                            } catch (IOException e) {
+                            } finally {
+
+                                try {
+                                    if (bw != null) {
+                                        bw.close();
+
+                                        Toast.makeText(getBaseContext(),
+                                                "Done writing both log and dump file",
+                                                Toast.LENGTH_SHORT).show();
 
 
-									}
-								} catch (Exception e2) {
-									e2.printStackTrace();
-								}
-							}
-				      }
-					};
-				 
-				  new Thread(runnable).start();
-				  
-				  Toast.makeText(getBaseContext(),
-							"Done writing to dump file",
-							Toast.LENGTH_SHORT).show();
-                stopService();
-			}
+                                    }
+                                } catch (Exception e2) {
+                                    e2.printStackTrace();
+                                }
+                            }
+                        }
+
+                    };
+
+
+                    new Thread(runnable).start();
+
+                    Toast.makeText(getBaseContext(),
+                            "Done writing to dump file",
+                            Toast.LENGTH_SHORT).show();
+                    stopService();
+                    break;
+
+                   case 1:
+                       Log.d("CLOSING","PACKET");
+                       stopService();
+                       Toast.makeText(getBaseContext(),
+                               "Done writing log file",
+                               Toast.LENGTH_SHORT).show();
+
+                       break;
+                    case 2:
+                        Log.d("CLOSING","RRC");
+                        Runnable runnable1 = new Runnable() {
+                            @Override
+                            public void run() {
+                                BufferedWriter bw = null;
+                                String line;
+                                String outText;
+                                int i;
+                                try {
+                                    File file1 = new File("mnt/sdcard/dump.txt");
+                                    file1.createNewFile();
+
+                                    OutputStreamWriter os = new OutputStreamWriter(
+                                            new FileOutputStream(file1));
+
+                                    bw = new BufferedWriter(os);
+
+                                    Process process = Runtime.getRuntime().exec(
+                                            "logcat -v threadtime -s ServiceMode");
+                                    int length = process.toString().length();
+
+                                    System.out.println("Length" + length);
+
+                                    BufferedReader reader = new BufferedReader(
+                                            new InputStreamReader(process.getInputStream()));
+
+                                    StringBuffer output1 = new StringBuffer();
+
+                                    while ((line = reader.readLine()) != null) {
+
+                                        bw.write(line);
+                                        bw.newLine();
+
+                                    }
+
+                                } catch (IOException e) {
+                                } finally {
+
+                                    try {
+                                        if (bw != null) {
+                                            bw.close();
+
+                                            Toast.makeText(getBaseContext(),
+                                                    "Done writing to dump file",
+                                                    Toast.LENGTH_SHORT).show();
+
+
+                                        }
+                                    } catch (Exception e2) {
+                                        e2.printStackTrace();
+                                    }
+                                }
+                            }
+
+                        };
+
+
+                        new Thread(runnable1).start();
+
+                        Toast.makeText(getBaseContext(),
+                                "Done writing to dump file",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+
+
+            }
 		});
 
 	}
@@ -212,4 +347,5 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
 }
